692e49735e7728fd25c276bbcd51367d0b1a8a55
premarumugam <prem.arumugam@commonground.work>
2024-10-04 13:54:20 +0800

    changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dbConnection = require('./config/db')
 const dotenv = require('dotenv').config()
+const appointmentRoutes = require('./routes/appointmentRoutes')
 
 dbConnection()
 const app = express()
@@ -8,7 +9,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.json())
-app.use('/api/appointments', require('./routes/appointmentRoutes'))
+app.use('/api/appointments', appointmentRoutes)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
