e22a215623fc50ff6cc45aebadd008bb4089355a
premarumugam <prem.arumugam@commonground.work>
2024-10-04 13:21:00 +0800

    changes: remove unused imports

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const connectDb = require('./config/db')
-// const errorHandler = require('./middleware/errorHandler')
 const dbConnection = require('./config/db')
 const dotenv = require('dotenv').config()
 
@@ -11,8 +9,6 @@ const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use('/api/appointments', require('./routes/appointmentRoutes'))
-// app.use('/api/users', require('./routes/userRoutes'))
-// app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
